f8f90fe9329d19ca255895ad4f513bb4be5d2cac
mikasa-404 <jhapriya106@gmail.com>
2024-07-05 11:33:52 +0530

    basic routing and assets added

---
+++ components/LeftSidebar.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+
+const LeftSidebar = () => {
+  const router = useRouter();
+  const pathName = usePathname();
+  return (
+    <section className="left_sidebar">
+      <nav className="flex flex-col gap-6">
+        <Link
+          href="/"
+          className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
+        >
+          <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+          <h1 className="text-24 font-extrabold text-white max-lg:hidden pl-2">
+            Castor
+          </h1>
+        </Link>
+        {sidebarLinks.map(({ label, imgUrl, route }) => {
+          const isActive =
+            pathName === route || pathName.startsWith(`${route}/`);
+          return (
+            <Link
+              href={route}
+              key={label}
+              className={cn(
+                "flex cursor-pointer items-center py-4 gap-3 max-lg:px-4 justify-center lg:justify-start",
+                { "bg-nav-focus border-r-4 border-green-1": isActive }
+              )}
+            >
+              <Image src={imgUrl} alt="logo" width={24} height={24} />
+              <p className=" font-extrabold text-white">{label}</p>
+            </Link>
+          );
+        })}
+      </nav>
+    </section>
+  );
+};
+
+export default LeftSidebar;
---
+++ constants/index.ts
@@ -0,0 +1,19 @@
+export const sidebarLinks = [
+   
+    
+    {
+      label: "Home",
+      imgUrl: "/icons/home.svg",
+      route: "/",
+    },
+    {
+        label: "Discover",
+        imgUrl: "/icons/discover.svg",
+        route: "/discover",
+    },
+   {
+    label: "Create Podcast",
+    imgUrl: "/icons/microphone.svg",
+    route: "/create-podcast",
+   }
+  ];
