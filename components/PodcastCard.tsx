d63df6ade765a763968011bf17cc778c1aa373d5
mikasa-404 <jhapriya106@gmail.com>
2024-07-10 18:48:19 +0530

    all podcast controllers addded

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,26 +1,31 @@
+import { PodcastCardProps } from "@/types";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const PodcastCard = ({
-  imgURL,
+  imgUrl,
   title,
   description,
   podcastId,
-}: {
-  imgURL: string;
-  title: string;
-  description: string;
-  podcastId: number;
-}) => {
+}: PodcastCardProps) => {
+  const router = useRouter();
+  const handleViews = ()=>{
+    //increase view count
+    router.push(`/podcasts/${podcastId}`,{
+      scroll: true
+    });
+  }
+
   return (
-    <div className="cursor-pointer">
-      <figure className="flex flex-col gap-2">
+    <div className="cursor-pointer" onClick={handleViews}>
+      <figure className="flex w-full h-auto flex-col gap-2">
         <Image
-          src={imgURL}
+          src={imgUrl}
           alt={title}
           width={174}
           height={174}
-          className="rounded-xl aspect-square h-fit w-full 2xl:size-[200px]"
+          className="rounded-xl aspect-square h-auto object-cover w-full"
         />
         <div className="flex flex-col">
           <h1 className="text-16 truncate font-bold text-white-1">{title}</h1>
---
+++ components/PodcastDetailPlayer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PodcastDetailPlayer = () => {
+  return (
+    <div className='text-white-1'>PodcastDetailPlayer</div>
+  )
+}
+
+export default PodcastDetailPlayer
